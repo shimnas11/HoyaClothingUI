cc1bd935f247827fd443352745284bdcc3386dbd
Shimnas11 <shimnassid@gmail.com>
2026-03-24 19:24:33 +0530

    update prod api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 
 export const environment = {
     production: true,
-    apiUrl: 'http://hoyaclothingco-001-site1.rtempurl.com/api'
+    apiUrl: 'https://hoyaclothingco-001-site1.rtempurl.com/api'
 };
